a33ab5056fd46c09833c75cb134a2d253cb06cf1
Sophie Townsend <se.townsend31@gmail.com>
2021-08-28 13:49:29 +1000

    git init file setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="main-heading">Star Wars API</h1>
+
+      <form action="">
+        <input type="text" />  
+        <button>search</button>
+      </form>    
+
+      <ul>
+        <li>Movie name</li>
+        <li>Movie name</li>
+        <li>Movie name</li>
+      </ul>
+
     </div>
   );
 }
