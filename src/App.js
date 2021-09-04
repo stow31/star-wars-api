ed4bf3bfd1a7a86110179e2cafc72ad7a567f878
Sophie Townsend <se.townsend31@gmail.com>
2021-09-04 17:05:55 +1000

    Revert "fixed bug merging the title and not getting the correct index when star is clicked"
    This reverts commit fcf838c6e8513f7c7d03ad37775be37ea3f2dc84.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const {
     searchText, 
     setSearchText, 
+    searchResults,
     swFilms, 
     searchResults,
     setSearchResults
@@ -25,6 +26,7 @@ function App() {
 
   // get the search results from the search text
   const getSearchResults = () =>{
+<<<<<<< HEAD
 
     let fullMovieList = JSON.parse(localStorage.getItem('swFilms'));
 
@@ -32,12 +34,20 @@ function App() {
       setSearchResults(JSON.parse(localStorage.getItem('swFilms')))
     } else {
       let filteredArr = fullMovieList
+=======
+    if (searchText.trim() === ""){
+      setSearchResults()
+    } else {
+      let filteredArr = swFilms
+      ?.map( (obj, idx) => {
+        return {...obj, original_index: idx}
+      })
+>>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
       ?.filter( obj => {
         if (obj.title.toUpperCase().includes(searchText.toUpperCase())) { 
           return obj
         }
       })
-      
       setSearchResults(filteredArr)
     }
   } 
@@ -45,8 +55,18 @@ function App() {
   return (
     <div className="App">
       <h1 className="main-heading">Star Wars API</h1>
+<<<<<<< HEAD
       <input className="search-box-input" onChange={handleTextChange} placeholder="Search Movies" type="text" />  
       <MovieList />
+=======
+      <input onChange={handleTextChange} placeholder="Search Movies" type="text" />  
+
+      { 
+        // searchResults ?  
+        // <SearchResults/> :
+        <MovieList />
+      }
+>>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
     </div>
   );
 }
--- src/MovieList.js
+++ src/MovieList.js
@@ -9,8 +9,7 @@ function MovieList() {
         setSwFilms,
         swFavFilms, 
         setSwFavFilms,
-        searchResults,
-        setSearchResults
+        searchResults
     } = useContext(SWContext);
 
 
@@ -19,6 +18,7 @@ function MovieList() {
         axios
         .get("https://swapi.dev/api/films")
         .then(res => {
+<<<<<<< HEAD
             let data = res.data.results.map( (obj, idx) => {
                 return {...obj, original_index: idx}
             })
@@ -28,6 +28,10 @@ function MovieList() {
             // setSwFilms(JSON.parse(localStorage.getItem('swFilms')))
 
             setSearchResults(JSON.parse(localStorage.getItem('swFilms')))
+=======
+            localStorage.setItem('swFilms', JSON.stringify(res.data.results))
+            setSwFilms(JSON.parse(localStorage.getItem('swFilms')))
+>>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
         })
         .catch(error => {
             console.log(error);
@@ -47,17 +51,25 @@ function MovieList() {
 
     // add or remove the movie into the fav movie list
     const updateMoviesWithFavs = (updateState, idx) =>{
+<<<<<<< HEAD
         localStorage.setItem('swFilms', JSON.stringify([...searchResults.slice(0, idx), {...searchResults[idx], favourite: updateState}, ...searchResults.slice(idx+1)]))
+=======
+
+        localStorage.setItem('swFilms', JSON.stringify([...swFilms.slice(0, idx), {...swFilms[idx], favourite: updateState}, ...swFilms.slice(idx+1)]))
+>>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
 
         setSearchResults(JSON.parse(localStorage.getItem('swFilms')))
     }
   
     // when the star is clicked 
     const handleStarClick = (idx) =>{
-        console.log(idx)
         if(swFavFilms.length > 0){
+<<<<<<< HEAD
             console.log(idx)
             let index = getFavListIndex(searchResults[idx]["title"])
+=======
+            let index = getFavListIndex(swFilms[idx]["title"])
+>>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
 
             if (index>-1){
                 //the movie is in the favourites list - you need to remove
@@ -84,28 +96,56 @@ function MovieList() {
 
             updateMoviesWithFavs(true, idx)
         }
+
+        // localStorage.setItem('swFilms', JSON.stringify(swFilms.sort((x, y) => {
+        //     return (x["favourite"] === y["favourite"])? 0 : x["favourite"] ? -1 : 1;
+        // })))
+        // setSwFilms(JSON.parse(localStorage.getItem('swFilms')))
     }
 
     return(<div>
 
         { 
+<<<<<<< HEAD
             // swFilms && 
             searchResults ? 
             searchResults
                 .sort((x, y) => {
+=======
+            swFilms ? 
+                searchResults ? 
+                    searchResults
+                    .sort((x, y) => {
+>>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
                     return (getFavListIndex(x["title"]) !== -1 && getFavListIndex(y["title"]) !== -1 || getFavListIndex(x["title"]) > -1 && getFavListIndex(y["title"]) > -1 ) ? 0 : getFavListIndex(x["title"]) > -1 ? -1 : 1;
-                })
-                .map( (obj, idx) => 
-                    <div className="movie-results-div">
-                        <span>{obj.title}</span>
-                        <span
-                            key={idx}xs
-                            onClick={ (e) => handleStarClick(obj.original_index)} 
-                            className={getFavListIndex(obj.title)>-1 ? "star highlighted" : "star"}> 
-                            ☆
-                        </span>
-                    </div> 
-                )
+                    })
+                    .map( (obj, idx) => 
+                        <div className="movie-results-div">
+                            <span>{obj.title}</span>
+                            <span
+                                key={idx}
+                                onClick={ (e) => handleStarClick(obj.original_index)} 
+                                className={getFavListIndex(obj.title)>-1 ? "star highlighted" : "star"}> 
+                                ☆
+                            </span>
+                        </div> )
+                :
+                    swFilms
+                        .sort((x, y) => {
+                            return (getFavListIndex(x["title"]) !== -1 && getFavListIndex(y["title"]) !== -1 || getFavListIndex(x["title"]) > -1 && getFavListIndex(y["title"]) > -1 ) ? 0 : getFavListIndex(x["title"]) > -1 ? -1 : 1;
+                        })
+                        .map( (obj, idx) => 
+                            <div className="movie-results-div">
+                                <span key={obj.episode_id}>{obj.title}</span>
+                                <span
+                                    key={idx}
+                                    id={idx}
+                                    onClick={ (e) => handleStarClick(idx)} 
+                                    className={getFavListIndex(obj.title)>-1 ? "star highlighted" : "star"}> 
+                                    ☆
+                                </span>
+                            </div> ) 
+                
             : 
                 <p>loading...</p> 
         }
