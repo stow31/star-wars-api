ce6686baa5ade00bd1dd65a423883ec5032cc545
Sophie Townsend <se.townsend31@gmail.com>
2021-09-04 20:06:36 +1000

    cleaned up bugs with only having one movie list variable

--- src/MovieList.js
+++ src/MovieList.js
@@ -9,7 +9,8 @@ function MovieList() {
         setSwFilms,
         swFavFilms, 
         setSwFavFilms,
-        searchResults
+        searchResults,
+        setSearchResults
     } = useContext(SWContext);
 
 
@@ -18,20 +19,18 @@ function MovieList() {
         axios
         .get("https://swapi.dev/api/films")
         .then(res => {
-<<<<<<< HEAD
-            let data = res.data.results.map( (obj, idx) => {
+            
+            console.log(res.data.results)
+
+            let apiData = res.data.results.map( (obj, idx) => {
                 return {...obj, original_index: idx}
             })
 
-            localStorage.setItem('swFilms', JSON.stringify(data))
+            console.log(apiData)
 
-            // setSwFilms(JSON.parse(localStorage.getItem('swFilms')))
-
-            setSearchResults(JSON.parse(localStorage.getItem('swFilms')))
-=======
-            localStorage.setItem('swFilms', JSON.stringify(res.data.results))
+            localStorage.setItem('swFilms', JSON.stringify(apiData))
             setSwFilms(JSON.parse(localStorage.getItem('swFilms')))
->>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
+            setSearchResults(JSON.parse(localStorage.getItem('swFilms')))
         })
         .catch(error => {
             console.log(error);
@@ -51,26 +50,17 @@ function MovieList() {
 
     // add or remove the movie into the fav movie list
     const updateMoviesWithFavs = (updateState, idx) =>{
-<<<<<<< HEAD
-        localStorage.setItem('swFilms', JSON.stringify([...searchResults.slice(0, idx), {...searchResults[idx], favourite: updateState}, ...searchResults.slice(idx+1)]))
-=======
 
         localStorage.setItem('swFilms', JSON.stringify([...swFilms.slice(0, idx), {...swFilms[idx], favourite: updateState}, ...swFilms.slice(idx+1)]))
->>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
 
-        setSearchResults(JSON.parse(localStorage.getItem('swFilms')))
+        setSwFilms(JSON.parse(localStorage.getItem('swFilms')))
     }
   
     // when the star is clicked 
     const handleStarClick = (idx) =>{
+        console.log(idx)
         if(swFavFilms.length > 0){
-<<<<<<< HEAD
-            console.log(idx)
-            let index = getFavListIndex(searchResults[idx]["title"])
-=======
             let index = getFavListIndex(swFilms[idx]["title"])
->>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
-
             if (index>-1){
                 //the movie is in the favourites list - you need to remove
                 console.log([...swFavFilms.slice(0, index), ...swFavFilms.slice(index+1)])
@@ -83,7 +73,7 @@ function MovieList() {
                 
             } else {
                 //the movie is not in the favourites list - you need to add
-                localStorage.setItem('swFavFilms', JSON.stringify([...swFavFilms, searchResults[idx]]))
+                localStorage.setItem('swFavFilms', JSON.stringify([...swFavFilms, swFilms[idx]]))
 
                 setSwFavFilms(JSON.parse(localStorage.getItem('swFavFilms')))
 
@@ -91,33 +81,21 @@ function MovieList() {
             }
         } else {
             // there is nothing on the fav list add this one movie
-            localStorage.setItem('swFavFilms', JSON.stringify([searchResults[idx]]))
+            localStorage.setItem('swFavFilms', JSON.stringify([swFilms[idx]]))
             setSwFavFilms(JSON.parse(localStorage.getItem('swFavFilms')))
 
             updateMoviesWithFavs(true, idx)
         }
 
-        // localStorage.setItem('swFilms', JSON.stringify(swFilms.sort((x, y) => {
-        //     return (x["favourite"] === y["favourite"])? 0 : x["favourite"] ? -1 : 1;
-        // })))
-        // setSwFilms(JSON.parse(localStorage.getItem('swFilms')))
     }
 
     return(<div>
 
         { 
-<<<<<<< HEAD
-            // swFilms && 
-            searchResults ? 
-            searchResults
-                .sort((x, y) => {
-=======
-            swFilms ? 
-                searchResults ? 
+            swFilms && searchResults ? 
                     searchResults
                     .sort((x, y) => {
->>>>>>> parent of fcf838c... fixed bug merging the title and not getting the correct index when star is clicked
-                    return (getFavListIndex(x["title"]) !== -1 && getFavListIndex(y["title"]) !== -1 || getFavListIndex(x["title"]) > -1 && getFavListIndex(y["title"]) > -1 ) ? 0 : getFavListIndex(x["title"]) > -1 ? -1 : 1;
+                        return (getFavListIndex(x["title"]) !== -1 && getFavListIndex(y["title"]) !== -1 || getFavListIndex(x["title"]) > -1 && getFavListIndex(y["title"]) > -1 ) ? 0 : getFavListIndex(x["title"]) > -1 ? -1 : 1;
                     })
                     .map( (obj, idx) => 
                         <div className="movie-results-div">
@@ -129,22 +107,6 @@ function MovieList() {
                                 ☆
                             </span>
                         </div> )
-                :
-                    swFilms
-                        .sort((x, y) => {
-                            return (getFavListIndex(x["title"]) !== -1 && getFavListIndex(y["title"]) !== -1 || getFavListIndex(x["title"]) > -1 && getFavListIndex(y["title"]) > -1 ) ? 0 : getFavListIndex(x["title"]) > -1 ? -1 : 1;
-                        })
-                        .map( (obj, idx) => 
-                            <div className="movie-results-div">
-                                <span key={obj.episode_id}>{obj.title}</span>
-                                <span
-                                    key={idx}
-                                    id={idx}
-                                    onClick={ (e) => handleStarClick(idx)} 
-                                    className={getFavListIndex(obj.title)>-1 ? "star highlighted" : "star"}> 
-                                    ☆
-                                </span>
-                            </div> ) 
                 
             : 
                 <p>loading...</p> 
