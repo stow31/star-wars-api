47c5d7c1caac068e531d3f00dbd71a56666fa669
Sophie Townsend <se.townsend31@gmail.com>
2021-10-06 14:05:30 +1100

    update dependency bugs for useEffect

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,24 +9,23 @@ function SearchBar(){
         swFilms, 
         setSearchResults
       } = useContext(SWContext);
-
-      // get the search results from the search text
-      const getSearchResults = () =>{
-        let filteredArr = swFilms
-        ?.filter( obj => {
-          if (obj.title.toUpperCase().includes(searchText.toUpperCase())) { 
-            return obj
-          }
-          return null
-        })
-        setSearchResults(filteredArr)
-      } 
-
     
       // when the search text changes, get the search results
       useEffect(() => {
-        getSearchResults()
-      }, [searchText]);
+        // get the search results from the search text
+        const getSearchResults = () =>{
+          let filteredArr = swFilms
+          ?.filter( obj => {
+            if (obj.title.toUpperCase().includes(searchText.toUpperCase())) { 
+              return obj
+            }
+            return null
+          })
+          setSearchResults(filteredArr)
+        };
+        
+        getSearchResults();
+      }, [searchText, swFilms, setSearchResults]);
     
       // when the search text changes set the search text value
       const handleTextChange = (e) =>{
